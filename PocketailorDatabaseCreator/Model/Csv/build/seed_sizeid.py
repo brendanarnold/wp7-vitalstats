58964411e2b056e5cd8ae1b081f7201b95e6a5e1
Brendan Arnold <brendanarnold@gmail.com>
2013-06-04 02:05:28 +0100

    Added bunch more brands

--- PocketailorDatabaseCreator/Model/Csv/build/seed_sizeid.py
+++ PocketailorDatabaseCreator/Model/Csv/build/seed_sizeid.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 
-START_DIR = 'Raw\\EileenFisher'
+START_DIR = 'Raw\\Firetrap'
 
 for path, dirs, fns in os.walk(START_DIR):
     if os.path.basename(path) in ['.', 'test', 'Backup']:
