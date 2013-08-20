f98610c53f50863801ed7741695f8f0bff50d78d
Brendan Arnold <brendanarnold@gmail.com>
2013-08-20 18:22:26 +0100

    Altered the EditProfile page to be more inline with the others in the redesign. Also removed the key measurements button from the ConversionsPage.

--- Pocketailor/View/Pages/ConversionsPage.xaml.cs
+++ Pocketailor/View/Pages/ConversionsPage.xaml.cs
@@ -23,18 +23,13 @@ namespace Pocketailor.View
 
             this.DataContext = App.VM;
 
-            App.VM.ConversionDataLoaded += (s) =>
-            {
-                this.AdRotatorControl.DefaultHouseAdBody = "Pocketailor.LocalAd";
-                this.AdRotatorControl.Invalidate();
-            };
+            this.IsKeyMeasurementsDisplayed = false;
 
         }
 
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-            this.AdRotatorControl.Invalidate();
             base.OnNavigatedTo(e);
             
 
@@ -63,7 +58,6 @@ namespace Pocketailor.View
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            this.AdRotatorControl.Dispose();
             base.OnNavigatedFrom(e);
             App.VM.SaveBlacklistedBrands();
         }
@@ -244,44 +238,7 @@ namespace Pocketailor.View
         //}
 
 
-        private void tooBigBtn_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            ConversionData c = (sender as Button).DataContext as ConversionData;
-            c.TweakSizeDown();
-        }
-
-        private void rightSizeBtn_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            if (App.VM.AllowFeedBack == null) this.PromptForFeedbackPermission();
-            ConversionData c = (sender as Button).DataContext as ConversionData;
-            c.IsAdjusting = false;
-            c.AcceptTweaks();
-        }
-
-        private void tooSmallBtn_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            ConversionData c = (sender as Button).DataContext as ConversionData;
-            c.TweakSizeUp();
-        }
-
-        private void PromptForFeedbackPermission()
-        {
-
-            MessageBoxResult res = MessageBox.Show("You can send your adjustments to the Pocketailor team anonymously over the web so we can make the results better next time. If you would like to help then click 'ok' below, if you would rather not then click 'cancel'."
-                    + Environment.NewLine + Environment.NewLine
-                    + "You can change this anytime in the settings.",
-                    "Allow feedback?",
-                    MessageBoxButton.OKCancel);
-            if (res == MessageBoxResult.OK)
-            {
-                App.VM.AllowFeedBack = true;
-            }
-            else
-            {
-                App.VM.AllowFeedBack = false;
-            }
-
-        }
+       
 
         private void conversionResultContainerGrid_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
@@ -298,7 +255,25 @@ namespace Pocketailor.View
 
         }
 
+       
+
         #endregion
+
+        private bool IsKeyMeasurementsDisplayed { get; set; }
+
+        private void showKeyMeasurementsBtn_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (this.IsKeyMeasurementsDisplayed)
+            {
+                this.IsKeyMeasurementsDisplayed = false;
+                VisualStateManager.GoToState(this, "hiddenKeyMeasurements", true);
+            }
+            else
+            {
+                this.IsKeyMeasurementsDisplayed = true;
+                VisualStateManager.GoToState(this, "displayedKeyMeasurements", true);
+            }
+        }
 
     }
 }
